84d063649943ed3255ec8f536cea77da0b913d96
Thinh La <thinhlq@smartosc.com>
2023-08-27 04:10:16 +0700

    Fix layout movie page

--- app/movie/page.tsx
+++ app/movie/page.tsx
@@ -39,14 +39,16 @@ export default function Movie() {
   }, [genreList]);
 
   return (
-    <div className="layout-movie flex gap-10 w-full m-auto max-w-[1920px] p-5 relative">
-      <div className="sidebar movie-sidebar rounded-lg w-full block xl:w-1/5">
-        <GenreList genres={genreList} />
-      </div>
-      <div className="main-content w-full xl:w-4/5">
-        {dataPopular && <CarouselSlider2 movieList={dataPopular} />}
+    <div className="layout-movie flex gap-2 xl:gap-10 w-full flex-col xl:items-end m-auto xl:max-w-[1920px] p-5 relative ">
+      <div className="main-container flex flex-col xl:flex-row w-full gap-5 xl:gap-10 max-w-screen-2xl m-auto">
+        <div className="sidebar movie-sidebar rounded-lg w-full block xl:w-1/5">
+          <GenreList genres={genreList} />
+        </div>
+        <div className="main-content flex flex-col max-w-full xl:w-4/5 min-h-screen align-top">
+          {dataPopular && <CarouselSlider2 movieList={dataPopular} />}
 
-        <InfiniteScrollMovie fromPage={1} toPage={5} />
+          <InfiniteScrollMovie fromPage={1} toPage={5} />
+        </div>
       </div>
     </div>
   );
